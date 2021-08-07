39e3fefbad8fbd2800548cff0a2e52a4697e2524
Sina yeganeh <71815150+sina-yeganeh@users.noreply.github.com>
2021-08-07 21:57:24 +0430

    edit somethink

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const sqlite3 = require('sqlite3').verbose()
 const path = require('path')
 
+const port = 8000;
+
 const dbPath = path.join(__dirname, 'data', 'books.db')
 const db = new sqlite3.Database(dbPath, (err) => {
-	if (err) return console.error(err.message)
-	console.log("Successful connection to the database.")
+	if (err) return console.error(`Can't connect to database:${err.message}`)
+	console.log("Connected to database as successfulu!")
 })
 
 db.run(`CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR NOT NULL, content TEXT, author VARCHAR NOT NULL, likes INTEGER NOT NULL)`, err => {
@@ -22,12 +24,13 @@ let app = express()
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 app.use('/public', express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => db.all('SELECT * FROM books ORDER BY id DESC LIMIT 5', (err, rows) => {
 	if (err) return console.error(err.message)
 	res.render('index', { url: req.url, books: rows })
 }))
+
 app.get('/search', (req, res) => db.all('SELECT * FROM books WHERE title LIKE ? ORDER BY id DESC', req.query.title, (err, rows) => {
 	if (err) return console.error(err.message)
 	res.render('search', {
@@ -36,22 +39,26 @@ app.get('/search', (req, res) => db.all('SELECT * FROM books WHERE title LIKE ? 
 		books: rows
 	})
 }))
+
 app.get('/best', (req, res) => {
 	// supposed to render best books
 	// but simply redirect to homepage for now
 	res.redirect('/')
 })
+
 app.get('/popular', (req, res) => {
 	// supposed to render most visited books
 	// but simply redirect to homepage for now
 	res.redirect('/')
 })
+
 app.get('/b(ook)?/:id', (req, res) => {
 	db.get('SELECT * FROM books WHERE id = ?', req.params.id, (err, row) => {
 		if (err) return console.error(err.message)
 		res.render('book', { url: req.url, book: row })
 	})
 })
+
 app.get('/author/:name', (req, res) => {
 	db.all('SELECT * FROM books WHERE author = ?', req.params.name, (err, row) => {
 		if (err) return console.error(err.message)
@@ -59,4 +66,4 @@ app.get('/author/:name', (req, res) => {
 	})
 })
 
-app.listen(8080)
+app.listen(port, () => { console.log(`OnBook is running on port ${port}`) }
